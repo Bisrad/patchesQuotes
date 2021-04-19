f9a06c082daffaae00076267985b2f8fd77ec090
Brad Thompson <bthompson6225@gmail.com>
2021-04-18 21:00:55 -0700

    Remove Files

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    consumer_key:         'CONSUMER_KEY',
-    consumer_secret:      'CONSUMER_SECRET',
-    access_token:         'ACCESS_TOKEN',
-    access_token_secret:  'ACCESS_TOKEN_SECRET',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  }
